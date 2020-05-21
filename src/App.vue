0517e2314cbd4d6adcd04929329e1258f0818ecf
Daniel Nery <danielpontesnery@gmail.com>
2020-05-21 00:14:31 -0300

    Primeira Versão do Projeto

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="error" dark >
+      <v-toolbar-title>Most Used Words</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <Home />
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+
+import Home from "./components/Home";
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Pill.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="pill">
+    <span class="name">{{ name }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+}
+</script>
+
+<style scoped>
+    .pill {
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+
+        background-color: rgb(99, 236, 195);
+        color: #fff;
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+
+    }
+
+    .pill .name {
+        margin-left: 15px;
+
+    }
+
+    .pill .amount {
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        width: 35px;
+        border-radius: 18px;
+
+        background-color: #fff;
+        color: rgb(99, 236, 195);
+
+        font-size: 14px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+</style>
